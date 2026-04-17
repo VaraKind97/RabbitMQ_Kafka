0ab895870e45598d0b0ee2b44c236b36378615a4
VaraKind97 <valadiva@mail.ru>
2026-04-18 01:28:18 +0300

    Delete credit-application-service/src directory

--- credit-application-service/src/main/java/ru/netology/creditapplicationservice/repository/CreditRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.netology.creditapplicationservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.netology.creditapplicationservice.entity.CreditEntity;
-
-@Repository
-public interface CreditRepository extends JpaRepository<CreditEntity, Long> {
-}
